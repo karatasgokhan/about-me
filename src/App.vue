b294bb18a68cde3b1ac0941fe95601b9fc50d387
gokhanKaratass <78848175+gokhanKaratass@users.noreply.github.com>
2022-06-13 20:48:50 +0300

    İnitial Project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld msg="About" userStatus="Logged" isLoggedIn="true" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import "./styles/global.css";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 v-if="isLoggedIn === 'true'">{{ msg }} {{ userStatus }}</h1>
+    <h1 v-if="isLoggedIn === 'false'">{{ msg }}</h1>
+    <h1 v-show="isLoggedIn === 'true'">{{ msg }} {{ userStatus }}</h1>
+    <h1 v-show="isLoggedIn === 'false'">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    userStatus: String,
+    isLoggedIn: Boolean,
+  },
+};
+</script>
